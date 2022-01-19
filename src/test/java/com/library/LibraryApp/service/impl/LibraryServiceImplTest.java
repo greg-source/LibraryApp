dee0064e753550aa5e9e4fa15bf8cb83f4cbab5e
Greg <realgregorygrytsyniak@gmail.com>
2022-01-19 12:21:58 +0200

    Tests finish

--- src/test/java/com/library/LibraryApp/service/impl/LibraryServiceImplTest.java
+++ src/test/java/com/library/LibraryApp/service/impl/LibraryServiceImplTest.java
@@ -1,28 +1,87 @@
 package com.library.LibraryApp.service.impl;
 
+import com.library.LibraryApp.repository.LibraryRepository;
+import com.library.LibraryApp.repository.entity.Book;
+import com.library.LibraryApp.repository.entity.Library;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
 class LibraryServiceImplTest {
+    @MockBean
+    private LibraryRepository libraryRepository;
+
+    @InjectMocks
+    private LibraryServiceImpl libraryService;
+
 
     @Test
     void findAll() {
+        List<Library> libraryList = new ArrayList();
+
+        Library libraryOne = Library.builder().libraryId(1l).book(Book.builder().build()).build();
+        Library libraryTwo = Library.builder().libraryId(1l).book(Book.builder().build()).build();
+        Library libraryThree = Library.builder().libraryId(1l).book(Book.builder().build()).build();
+
+        libraryList.add(libraryOne);
+        libraryList.add(libraryTwo);
+        libraryList.add(libraryThree);
+
+        Mockito.when(libraryRepository.findAll()).thenReturn(libraryList);
+
+        List<Library> resultList = libraryService.findAll();
+
+        assertEquals(3, resultList.size());
+
     }
 
     @Test
     void findById() {
+        Library libraryOne = Library.builder().libraryId(1l).book(Book.builder().build()).build();
+
+        Mockito.doReturn(libraryOne).when(libraryRepository).findByLibraryId(1l);
+
+        Library resultLibraryOne = libraryService.findById(1l);
+
+        assertEquals(resultLibraryOne, libraryOne);
+
     }
 
     @Test
     void insert() {
+        Library libraryOne = Library.builder().libraryId(1l).book(Book.builder().build()).build();
+
+        libraryService.insert(libraryOne);
+
+        Mockito.verify(libraryRepository, Mockito.times(1)).save(libraryOne);
     }
 
     @Test
     void deleteById() {
+        Library libraryOne = Library.builder().libraryId(1l).book(Book.builder().build()).build();
+        libraryRepository.save(libraryOne);
+        libraryService.deleteById(1l);
+        Mockito.verify(libraryRepository, Mockito.times(1)).deleteById(1l);
+
     }
 
     @Test
     void update() {
+        Library libraryOne = Library.builder().libraryId(1l).book(Book.builder().build()).build();
+
+        libraryService.insert(libraryOne);
+
+        Mockito.verify(libraryRepository, Mockito.times(1)).save(libraryOne);
     }
 }
--- src/test/java/com/library/LibraryApp/service/impl/UserServiceImplTest.java
+++ src/test/java/com/library/LibraryApp/service/impl/UserServiceImplTest.java
@@ -1,28 +1,86 @@
 package com.library.LibraryApp.service.impl;
 
+import com.library.LibraryApp.repository.UserRepository;
+import com.library.LibraryApp.repository.entity.User;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
 class UserServiceImplTest {
+    @MockBean
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
 
     @Test
     void findAll() {
+        List<User> userList = new ArrayList();
+
+        User userOne = User.builder().userId(1l).firstName("John").lastName("Will").status("offline").build();
+        User userTwo = User.builder().userId(2l).firstName("John2").lastName("Will").status("offline").build();
+        User userThree = User.builder().userId(3l).firstName("John3").lastName("Will").status("offline").build();
+
+        userList.add(userOne);
+        userList.add(userTwo);
+        userList.add(userThree);
+
+        Mockito.when(userRepository.findAll()).thenReturn(userList);
+
+        List<User> resultList = userService.findAll();
+
+        assertEquals(3, resultList.size());
+
     }
 
     @Test
     void findById() {
+        User userOne = User.builder().userId(1l).firstName("John").lastName("Will").status("offline").build();
+
+        Mockito.doReturn(userOne).when(userRepository).findByUserId(1l);
+
+        User resultUserOne = userService.findById(1l);
+
+        assertEquals(resultUserOne, userOne);
+
     }
 
     @Test
     void insert() {
+        User userOne = User.builder().userId(1l).firstName("John").lastName("Will").status("offline").build();
+
+        userService.insert(userOne);
+
+        Mockito.verify(userRepository, Mockito.times(1)).save(userOne);
     }
 
     @Test
     void deleteById() {
+        User userOne = User.builder().userId(1l).firstName("John").lastName("Will").status("offline").build();
+        userRepository.save(userOne);
+        userService.deleteById(1l);
+        Mockito.verify(userRepository, Mockito.times(1)).deleteById(1l);
+
     }
 
     @Test
     void update() {
+        User userOne = User.builder().userId(1l).firstName("John").lastName("Will").status("offline").build();
+
+        userService.insert(userOne);
+
+        Mockito.verify(userRepository, Mockito.times(1)).save(userOne);
     }
 }
