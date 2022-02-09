0b6adc146a532115c4a3b6d24385653753f94923
Greg <realgregorygrytsyniak@gmail.com>
2022-02-09 17:09:07 +0200

    deploy helm

--- deploy/script.sh
+++ deploy/script.sh
@@ -1,14 +1,14 @@
-kubectl apply -f db-pvc.yaml  
+## kubectl apply -f db-pvc.yaml
 
-kubectl apply -f db-pv.yaml
+## kubectl apply -f db-pv.yaml
 
-kubectl apply -f db-secret.yaml
+## kubectl apply -f db-secret.yaml
 
-kubectl apply -f db-service.yaml
+## kubectl apply -f db-service.yaml
 
-kubectl apply -f db-deployment.yaml
-sleep 14
-kubectl apply -f app-service.yaml
+## kubectl apply -f db-deployment.yaml
+## sleep 14
+## kubectl apply -f app-service.yaml
 
-kubectl apply -f app-deployment.yaml
+## kubectl apply -f app-deployment.yaml
 
